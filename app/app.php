91ce2c25447b36f563eb4e3635855664d85214ed
Rob Strong <robert.a.strong@gmail.com>
2013-01-23 01:00:56 -0500

    clening up

--- app/app.php
+++ app/app.php
@@ -3,7 +3,11 @@
 require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new Silex\Application();
-$app['debug'] = true;
+$env = 'prod';
+
+if ($env != 'prod') {
+    $app['debug'] = true;
+}
 
 
 /*************************
@@ -13,20 +17,33 @@ $app['debug'] = true;
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__ . '/../app/views',
 ));
-/*
-$app->register(new SilexAssetic\AsseticExtension(), array(
-    'assetic.class_path' => __DIR__.'/vendor/assetic/src',
-    'assetic.path_to_web' => __DIR__ . '/../web',
-    'assetic.options' => array(
-        'debug' => TRUE
-    ),
-    'assetic.filters' => $app->protect(function($fm) {
-        $fm->set('scss', new Assetic\Filter\Sass\ScssFilter(
-            '/usr/bin/scss'
-        ));
-    })
-));
- */
+
+if ($env != 'prod') {
+    $app->register(new SilexAssetic\AsseticExtension(), array(
+        'assetic.class_path' => __DIR__.'/vendor/assetic/src',
+        'assetic.path_to_web' => __DIR__ . '/../web',
+        'assetic.options' => array(
+            'formulae_cache_dir' => __DIR__ . '/assetic/cache',
+            'debug' => TRUE
+        ),
+        'assetic.filters' => $app->protect(function($fm) {
+            $fm->set('scss', new Assetic\Filter\Sass\ScssFilter(
+                '/usr/bin/scss'
+            ));
+        }),
+        'assetic.assets' => $app->protect(function($am, $fm) {
+            $am->set('styles', new Assetic\Asset\AssetCache(
+                new Assetic\Asset\GlobAsset(
+                    __DIR__ . '/assets/css/*.scss',
+                    array($fm->get('scss'))
+                ),
+                new Assetic\Cache\FilesystemCache(__DIR__ . '/assetic/cache')
+            ));
+            $am->get('styles')->setTargetPath('/css/styles.css');
+        })
+    ));
+}
+
 /*******************
 * SETUP ROUTING
 *******************/
